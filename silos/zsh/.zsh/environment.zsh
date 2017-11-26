0637b9ceb835b24b40097c78bc728c940efa1cf7
Adam Rutkowski <hq@mtod.org>
2017-11-26 11:36:35 +0100

    Update zsh

--- silos/zsh/.zsh/environment.zsh
+++ silos/zsh/.zsh/environment.zsh
@@ -10,7 +10,7 @@ export MANPAGER="col -b | nvim -c 'setf man ts=4 nomod nolist noma' -"
 export NVIM_TUI_ENABLE_TRUE_COLOR=1
 export PAGER=less
 export PATH=$PATH:/Applications/Postgres.app/Contents/Versions/9.4/bin
-export PATH=/Users/hq1/bin:/usr/local/bin:$PATH
+export PATH=/Users/hq1/bin:/usr/local/bin:/usr/local/sbin:$PATH
 export ZSH_CACHE="$HOME/.zsh_cache"
 mkdir -p $ZSH_CACHE
 
