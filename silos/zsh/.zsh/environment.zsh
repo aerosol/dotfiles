f1162cc85e54f39ceeba4c6758131c62ee696c75
Adam Rutkowski <hq@mtod.org>
2018-12-21 23:45:46 +0100

    Update zsh

--- silos/zsh/.zsh/environment.zsh
+++ silos/zsh/.zsh/environment.zsh
@@ -13,8 +13,14 @@ unset DBUS_SESSION_BUS_ADDRESS
 
 source $HOME/.asdf/asdf.sh
 
+if [[ $(ps --no-header -p $PPID -o comm) =~ '^kitty|tmux$' ]]; then
+        for wid in $(xdotool search --pid $PPID); do
+            xprop -f _KDE_NET_WM_BLUR_BEHIND_REGION 32c -set _KDE_NET_WM_BLUR_BEHIND_REGION 0 -id $wid; done
+fi
+
 if [[ -z "$TMUX" ]] && [ "$SSH_CONNECTION" != "" ]; then
 	tmux attach-session -t ssh_tmux || tmux new-session -s ssh_tmux
 elif [[ -z "$TMUX" ]]; then
 	tmux new-session -s $(grep -E '^.{4,6}$' /usr/share/dict/american-english | shuf -n 1 | sed "s/'//g")
 fi
+
