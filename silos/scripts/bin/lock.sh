874e16908bec52ffcb1e0dbfb46ab3b16b6a4818
Adam Rutkowski <hq@mtod.org>
2019-12-08 18:32:10 +0100

    Update scripts

--- silos/scripts/bin/lock.sh
+++ silos/scripts/bin/lock.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-sxlock -f "-misc-fixed-medium-r-*--47-120-*-*-*-*-iso8859-1" -u "pass or gtfo"
-
+sxlock -f "-misc-fixed-medium-r-*--47-120-*-*-*-*-iso8859-1" -u "pass or gtfo" &
+notify-send "Locked"
