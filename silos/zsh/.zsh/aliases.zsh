7cf2bf1e29f3ee901a8473291624d6459fbc9847
Adam Rutkowski <hq@mtod.org>
2021-03-04 13:12:56 +0100

    Update zsh

--- silos/zsh/.zsh/aliases.zsh
+++ silos/zsh/.zsh/aliases.zsh
@@ -12,12 +12,13 @@ alias gst='git st'
 alias gco='git checkout'
 alias gp='git pub'
 alias gf='git fetch'
-alias grim='git fetch && git checkout master && git pull origin master && git checkout - && git rebase -i origin/master'
+alias grim='git fetch && git checkout main && git pull origin main && git checkout - && git rebase -i origin/main'
 alias grc='git rebase --continue'
 alias ag='rg'
 alias vim='nvim'
 alias v='nvim'
 alias d="kitty +kitten diff"
+alias yt="ytfzf -t -s"
 
 autoload zmv
 alias zmv='noglob zmv'
--- silos/zsh/.zsh/environment.zsh
+++ silos/zsh/.zsh/environment.zsh
@@ -9,6 +9,10 @@ export PAGER=less
 export TERMINAL=kitty
 export PATH=$HOME/.local/bin:$HOME/bin:$PATH
 
+export YTFZF_HIST=1
+export YTFZF_LOOP=1
+export YTFZF_PREF="bestvideo[height<=?1080]+bestaudio/best"
+
 if [[ "$TERM" == "xterm-kitty" ]]; then
 	if [[ -z "$TMUX" ]] && [ "$SSH_CONNECTION" != "" ]; then
 		tmux attach-session -t ssh_tmux || tmux new-session -s ssh_tmux
