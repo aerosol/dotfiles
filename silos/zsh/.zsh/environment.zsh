2385bcc82277950f243287cfaaaafdbb11e3c168
Adam Rutkowski <hq@mtod.org>
2022-12-02 13:58:08 +0100

    Update zsh

--- silos/zsh/.zsh/environment.zsh
+++ silos/zsh/.zsh/environment.zsh
@@ -7,7 +7,7 @@ export LANG=en_US.UTF-8
 export LC_ALL=en_US.UTF-8
 export PAGER=less
 export TERMINAL=alacritty
-export PATH=$HOME/.local/bin:$HOME/bin:$PATH
+export PATH=$HOME/.local/bin:$HOME/bin:$HOME/.nimble/bin:$PATH
 export ZK_NOTEBOOK_DIR=~/notebooks/
 export EXA_ICON_SPACING=2
 
