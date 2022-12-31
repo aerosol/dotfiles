1a5a6fe86ad17c71a802a55a66f1ab0bf849b8f2
Adam Rutkowski <hq@mtod.org>
2022-12-31 18:20:57 +0100

    Update zsh

--- silos/zsh/.zsh/environment.zsh
+++ silos/zsh/.zsh/environment.zsh
@@ -7,12 +7,12 @@ export ERL_AFLAGS="-kernel shell_history enabled"
 export LANG=en_US.UTF-8
 export LC_ALL=en_US.UTF-8
 export PAGER=less
-export TERMINAL=alacritty
+export TERMINAL=kitty
 export PATH=$HOME/.local/bin:$HOME/bin:$HOME/.nimble/bin:$PATH
 export ZK_NOTEBOOK_DIR=~/notebooks/
 export EXA_ICON_SPACING=2
 
-if [[ "$TERM" == "xterm-256color" ]]; then
+if [[ "$TERM" == "xterm-kitty" ]]; then
 	if [[ -z "$TMUX" ]] && [ "$SSH_CONNECTION" != "" ]; then
 		tmux attach-session -t ssh_tmux || tmux new-session -s ssh_tmux
 	elif [[ -z "$TMUX" ]]; then
