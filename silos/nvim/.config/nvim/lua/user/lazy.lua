f9ef7ddf34d655659ca823571d3c90beab5947c7
Adam Rutkowski <hq@mtod.org>
2023-01-09 22:16:12 +0100

    Update nvim

--- silos/nvim/.config/nvim/lua/user/lazy.lua
+++ silos/nvim/.config/nvim/lua/user/lazy.lua
@@ -98,17 +98,40 @@ local plugins = {
 			local fzf = require("fzf-lua")
 			local opts = { noremap = true, silent = true, desc = "FZF Related Shit" }
 
-			fzf.setup({ winopts = { fullscreen = true, preview = {} } })
+			fzf.setup({
+				fzf_opts = {
+					['--layout'] = 'reverse'
+				},
+				winopts = {
+					fullscreen = true,
+					preview = {
+						layout = 'flex',
+						default = 'bat_native'
+					}
+				}
+			})
 
 			vim.keymap.set("n", "go", function()
-				fzf.files()
+				fzf.files({
+					winopts = { preview = { hidden = 'hidden' } },
+				})
+			end, opts)
+
+			vim.keymap.set("n", "gO", function()
+				fzf.files({
+					winopts = { preview = { hidden = 'hidden' } },
+					fd_opts = [[--color=never --type f --hidden --follow --no-ignore -E .git -E _build -E .elixir_ls]]
+				})
 			end, opts)
 
 			vim.keymap.set("n", "<leader>l", function()
 				fzf.live_grep_native()
 			end, opts)
 
-			vim.keymap.set("n", "<leader>L", function() fzf.live_grep_native({ rg_opts = '--no-ignore' }) end, opts)
+			vim.keymap.set("n", "<leader>L", function() fzf.live_grep_native({
+					rg_opts = [[--column --line-number --no-heading --color=always --smart-case --max-columns=512 --no-ignore -g '!{.git,_build,.elixir_ls}/']]
+				})
+			end, opts)
 
 			vim.keymap.set("n", "<leader><space>", function()
 				fzf.buffers()
