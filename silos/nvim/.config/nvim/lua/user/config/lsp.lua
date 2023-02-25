160d5386860d0352edabaa6084f33d95bb4e1994
Adam Rutkowski <hq@mtod.org>
2023-02-25 13:17:56 +0100

    Update nvim

--- silos/nvim/.config/nvim/lua/user/config/lsp.lua
+++ silos/nvim/.config/nvim/lua/user/config/lsp.lua
@@ -56,7 +56,7 @@ local servers = {
 	rust_analyzer = {},
 	tsserver = {},
 
-	sumneko_lua = {
+	lua_ls = {
 		Lua = {
 			workspace = { checkThirdParty = false },
 			telemetry = { enable = false },
