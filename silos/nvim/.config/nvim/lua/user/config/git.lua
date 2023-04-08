447031b8f6a433d1b8d74bf4727b7c73161a42bb
Adam Rutkowski <hq@mtod.org>
2023-04-08 21:15:04 +0200

    Update nvim

--- silos/nvim/.config/nvim/lua/user/config/git.lua
+++ silos/nvim/.config/nvim/lua/user/config/git.lua
@@ -2,5 +2,5 @@ local keymap = vim.keymap.set
 local opts = { noremap = true, silent = true }
 
 keymap("n", "gs", ":Git<cr>", opts)
-keymap("n", "gb", ":Git blame<cr>", opts)
-keymap("n", "gB", ":GBrowse<cr>", opts)
+keymap({ "n", "v" }, "gb", ":Git blame<cr>", opts)
+keymap({ "n", "v" }, "gB", ":GBrowse<cr>", opts)
