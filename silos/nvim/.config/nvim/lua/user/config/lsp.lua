cbbe86fddec024ea62b9ce8cf287d7af73735b97
Adam Rutkowski <hq@mtod.org>
2023-04-10 15:05:24 +0200

    Update nvim

--- silos/nvim/.config/nvim/lua/user/config/lsp.lua
+++ silos/nvim/.config/nvim/lua/user/config/lsp.lua
@@ -112,7 +112,7 @@ cmp.setup({
 		end,
 	},
 	mapping = cmp.mapping.preset.insert({
-		["<C-d>"] = cmp.mapping.scroll_docs( -4),
+		["<C-d>"] = cmp.mapping.scroll_docs(-4),
 		["<C-f>"] = cmp.mapping.scroll_docs(4),
 		["<C-Space>"] = cmp.mapping.complete(),
 		["<CR>"] = cmp.mapping.confirm({
@@ -131,8 +131,8 @@ cmp.setup({
 		["<S-Tab>"] = cmp.mapping(function(fallback)
 			if cmp.visible() then
 				cmp.select_prev_item()
-			elseif luasnip.jumpable( -1) then
-				luasnip.jump( -1)
+			elseif luasnip.jumpable(-1) then
+				luasnip.jump(-1)
 			else
 				fallback()
 			end
@@ -141,5 +141,6 @@ cmp.setup({
 	sources = {
 		{ name = "nvim_lsp" },
 		{ name = "luasnip" },
+		{ name = "buffer" }
 	},
 })
--- silos/nvim/.config/nvim/lua/user/lazy.lua
+++ silos/nvim/.config/nvim/lua/user/lazy.lua
@@ -80,7 +80,12 @@ local plugins = {
 	"tpope/vim-sleuth",
 	{
 		"hrsh7th/nvim-cmp",
-		dependencies = { "hrsh7th/cmp-nvim-lsp", "L3MON4D3/LuaSnip", "saadparwaiz1/cmp_luasnip" },
+		dependencies = {
+			"hrsh7th/cmp-nvim-lsp",
+			"L3MON4D3/LuaSnip",
+			"saadparwaiz1/cmp_luasnip",
+			"hrsh7th/cmp-buffer"
+		},
 	},
 	{
 		"ibhagwan/fzf-lua",
