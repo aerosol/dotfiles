effa31be0ce40f507ef90ac5092b2c88a05b58f8
Adam Rutkowski <hq@mtod.org>
2023-04-10 15:07:48 +0200

    Update nvim

--- silos/nvim/.config/nvim/lua/user/config/lsp.lua
+++ silos/nvim/.config/nvim/lua/user/config/lsp.lua
@@ -144,3 +144,19 @@ cmp.setup({
 		{ name = "buffer" }
 	},
 })
+
+cmp.setup.cmdline({ '/', '?' }, {
+	mapping = cmp.mapping.preset.cmdline(),
+	sources = {
+		{ name = 'buffer' }
+	}
+})
+
+cmp.setup.cmdline(':', {
+	mapping = cmp.mapping.preset.cmdline(),
+	sources = cmp.config.sources({
+		{ name = 'path' }
+	}, {
+		{ name = 'cmdline' }
+	})
+})
--- silos/nvim/.config/nvim/lua/user/lazy.lua
+++ silos/nvim/.config/nvim/lua/user/lazy.lua
@@ -84,7 +84,8 @@ local plugins = {
 			"hrsh7th/cmp-nvim-lsp",
 			"L3MON4D3/LuaSnip",
 			"saadparwaiz1/cmp_luasnip",
-			"hrsh7th/cmp-buffer"
+			"hrsh7th/cmp-buffer",
+			"hrsh7th/cmp-cmdline",
 		},
 	},
 	{
