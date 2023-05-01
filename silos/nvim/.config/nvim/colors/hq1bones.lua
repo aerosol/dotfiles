5951a76553250fa6b2838f47abba88d8047c974b
Adam Rutkowski <hq@mtod.org>
2023-05-01 08:41:43 +0200

    Update nvim

--- silos/nvim/.config/nvim/colors/hq1bones.lua
+++ silos/nvim/.config/nvim/colors/hq1bones.lua
@@ -1,9 +1,9 @@
 local colors_name = "hq1bones"
 vim.g.colors_name = colors_name -- Required when defining a colorscheme
 
-local lush = require "lush"
+local lush = require("lush")
 local hsluv = lush.hsluv -- Human-friendly hsl
-local util = require "zenbones.util"
+local util = require("zenbones.util")
 
 local bg = vim.o.background
 
@@ -11,25 +11,25 @@ local bg = vim.o.background
 -- Based on https://github.com/gruvbox-community/gruvbox#palette
 local palette
 palette = util.palette_extend({
-	bg = hsluv "#000000",
-	fg = hsluv "#C2C3C7",
-	rose = hsluv "#7E2553",
-	leaf = hsluv "#008751",
-	wood = hsluv "#AB5236",
-	water = hsluv "#5f574f",
-	blossom = hsluv "#FF77A8",
-	sky = hsluv "#83769C",
+	bg = hsluv("#000000"),
+	fg = hsluv("#C2C3C7"),
+	rose = hsluv("#7E2553"),
+	leaf = hsluv("#008751"),
+	wood = hsluv("#AB5236"),
+	water = hsluv("#5f574f"),
+	blossom = hsluv("#FF77A8"),
+	sky = hsluv("#83769C"),
 }, bg)
 
 -- Generate the lush specs using the generator util
-local generator = require "zenbones.specs"
+local generator = require("zenbones.specs")
 local base_specs = generator.generate(palette, bg, generator.get_global_config(colors_name, bg))
 
 -- Optionally extend specs using Lush
 local specs = lush.extends({ base_specs }).with(function()
 	return {
-		Special { fg = palette.water },
-		Type { fg = palette.sky, gui = "italic" }
+		Special({ fg = palette.water }),
+		Type({ fg = palette.sky, gui = "italic" }),
 	}
 end)
 
