8891b5ba602017242947897fa2f01f2862ed72a0
Adam Rutkowski <hq@mtod.org>
2023-05-07 20:26:18 +0200

    Update zsh

--- silos/zsh/.zsh/prompt.zsh
+++ silos/zsh/.zsh/prompt.zsh
@@ -1,5 +1,4 @@
 zstyle ':vcs_info:git:*' formats '%b '
 precmd() { vcs_info }
 PROMPT='
-%F{white}%D{%T}%f %F{cyan}%M %f%F{green}%~%f %(?..%F{red}⛈ %? %f)
-%F{blue}${vcs_info_msg_0_}%f$ '
+%F{cyan}%M %f%F{green}%~%f %(?..%F{red}⛈ %? %f)%F{blue}${vcs_info_msg_0_}%f$ '
