6654db54e69ee3a49e3684b7d5092a6c78dc6538
Adam Rutkowski <hq@mtod.org>
2023-05-16 22:34:06 +0200

    Update nvim

--- silos/nvim/.config/nvim/lua/hq1/plugins/lines.lua
+++ silos/nvim/.config/nvim/lua/hq1/plugins/lines.lua
@@ -5,6 +5,7 @@ return {
 		require("lualine").setup({
 			options = {
 				icons_enabled = true,
+				theme = "auto",
 				component_separators = { left = "", right = "" },
 				section_separators = { left = "", right = "" },
 				disabled_filetypes = {
