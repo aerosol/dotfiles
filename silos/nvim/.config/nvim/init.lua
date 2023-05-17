39943456d58e2875c026986ce5a97634f2e5e21d
Adam Rutkowski <hq@mtod.org>
2023-05-17 22:12:49 +0200

    Update nvim

--- silos/nvim/.config/nvim/init.lua
+++ silos/nvim/.config/nvim/init.lua
@@ -1,8 +1,8 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
+-- vim.g.loaded_netrw = 1
+-- vim.g.loaded_netrwPlugin = 1
 
 require("hq1.lazy")
 require("lazy").setup("hq1.plugins", {})
--- silos/nvim/.config/nvim/lua/hq1/plugins/colors.lua
+++ silos/nvim/.config/nvim/lua/hq1/plugins/colors.lua
@@ -2,6 +2,17 @@ return {
 	{
 		"rebelot/kanagawa.nvim",
 		config = function()
+			require("kanagawa").setup({
+				colors = {
+					theme = {
+						all = {
+							ui = {
+								bg_gutter = "none",
+							},
+						},
+					},
+				},
+			})
 			if os.getenv("MUH_THEME") == "dark" then
 				vim.cmd("colorscheme kanagawa-dragon")
 			else
