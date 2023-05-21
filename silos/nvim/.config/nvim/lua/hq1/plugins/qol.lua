c16a505ed38937199b80f69f260346d24e476306
Adam Rutkowski <hq@mtod.org>
2023-05-21 07:37:10 +0200

    Update nvim

--- silos/nvim/.config/nvim/lua/hq1/plugins/qol.lua
+++ silos/nvim/.config/nvim/lua/hq1/plugins/qol.lua
@@ -1,4 +1,9 @@
 return {
 	"tpope/vim-sleuth",
 	"andymass/vim-matchup",
+	{
+		"theRealCarneiro/hyprland-vim-syntax",
+		dependencies = { "nvim-treesitter/nvim-treesitter" },
+		ft = "hypr",
+	},
 }
