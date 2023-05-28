2cbb06d8993354ceb573d9bfda82b8a834cb33f5
Adam Rutkowski <hq@mtod.org>
2023-05-28 19:28:36 +0200

    Update zsh

--- silos/zsh/.zsh/prompt.zsh
+++ silos/zsh/.zsh/prompt.zsh
@@ -5,4 +5,12 @@ function precmd () {
 		vcs_info
 }
 
+function precmd {
+	print -Pn "\e]0;zsh%L %(1j,%j job%(2j|s|); ,)%~\a"
+}
+
+function preexec {
+	printf "\033]0;%s\a" "$@"
+}
+
 PROMPT='%F{cyan}%M %f%F{green}%~%f %(?..%F{red}⛈ %? %f)%F{blue}${vcs_info_msg_0_}%f$ '
