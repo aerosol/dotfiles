0811c24cf4e151a82656c6f96ab5cc6ed0985b2b
Adam Rutkowski <hq@mtod.org>
2023-10-29 09:08:40 +0100

    Update zsh

--- silos/zsh/.zsh/prompt.zsh
+++ silos/zsh/.zsh/prompt.zsh
@@ -4,6 +4,4 @@ precmd() { vcs_info }
 function precmd () {
 		vcs_info
 }
-setopt PROMPT_SUBST
-
-PROMPT='%F{cyan}%M$f:%F{blue}$(tmux display-message -p "#S") %f%F{green}%~%f %(?..%F{red}⛈ %? %f)%F{blue}${vcs_info_msg_0_}%f$ '
+PROMPT='%F{cyan}%M%f %F{green}%~%f %(?..%F{red}⛈ %? %f)%F{blue}${vcs_info_msg_0_}%f$ '
