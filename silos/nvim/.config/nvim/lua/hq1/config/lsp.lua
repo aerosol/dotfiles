94441849e2d8819cf783e2e04159cf144253f615
Adam Rutkowski <hq@mtod.org>
2023-12-07 10:44:49 +0100

    Update nvim

--- silos/nvim/.config/nvim/lua/hq1/config/lsp.lua
+++ silos/nvim/.config/nvim/lua/hq1/config/lsp.lua
@@ -166,7 +166,6 @@ mason_lspconfig.setup_handlers({
 
 -- nvim-cmp setup
 local cmp = require("cmp")
-local luasnip = require("luasnip")
 local lspkind = require("lspkind")
 
 lspkind.init({
@@ -194,11 +193,6 @@ cmp.setup({
 	formatting = {
 		format = lspkind.cmp_format(),
 	},
-	snippet = {
-		expand = function(args)
-			luasnip.lsp_expand(args.body)
-		end,
-	},
 	mapping = cmp.mapping.preset.insert({
 		["<C-d>"] = cmp.mapping.scroll_docs(-4),
 		["<C-f>"] = cmp.mapping.scroll_docs(4),
@@ -210,8 +204,6 @@ cmp.setup({
 		["<Tab>"] = cmp.mapping(function(fallback)
 			if cmp.visible() then
 				cmp.select_next_item()
-			elseif luasnip.expand_or_jumpable() then
-				luasnip.expand_or_jump()
 			else
 				fallback()
 			end
@@ -219,8 +211,6 @@ cmp.setup({
 		["<S-Tab>"] = cmp.mapping(function(fallback)
 			if cmp.visible() then
 				cmp.select_prev_item()
-			elseif luasnip.jumpable(-1) then
-				luasnip.jump(-1)
 			else
 				fallback()
 			end
@@ -230,7 +220,6 @@ cmp.setup({
 		{ name = "codeium",  group_index = 2 },
 		{ name = "copilot",  group_index = 2 },
 		{ name = "nvim_lsp", group_index = 2 },
-		{ name = "luasnip",  group_index = 2 },
 		{ name = "buffer",   group_index = 2 },
 	},
 })
--- silos/nvim/.config/nvim/lua/hq1/plugins/lsp.lua
+++ silos/nvim/.config/nvim/lua/hq1/plugins/lsp.lua
@@ -49,8 +49,6 @@ return {
 		"hrsh7th/nvim-cmp",
 		dependencies = {
 			"hrsh7th/cmp-nvim-lsp",
-			"L3MON4D3/LuaSnip",
-			"saadparwaiz1/cmp_luasnip",
 			"hrsh7th/cmp-buffer",
 			"hrsh7th/cmp-cmdline",
 		},
