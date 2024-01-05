3a9cb0f690bf5a3cf28aedb85e3d3154a2db4509
Adam Rutkowski <hq@mtod.org>
2024-01-05 07:24:17 +0100

    Update scripts

--- silos/scripts/bin/cpu.sh
+++ silos/scripts/bin/cpu.sh
@@ -1,8 +1,9 @@
-	#!/usr/bin/env bash
-	read -r cpu a b c previdle rest < /proc/stat
+#!/usr/bin/env bash
+
+read -r cpu a b c previdle rest < /proc/stat
 prevtotal=$((a+b+c+previdle))
-	sleep 0.5
-	read -r cpu a b c idle rest < /proc/stat
-	total=$((a+b+c+idle))
+sleep 0.5
+read -r cpu a b c idle rest < /proc/stat
+total=$((a+b+c+idle))
 cpu=$((100*( (total-prevtotal) - (idle-previdle) ) / (total-prevtotal) ))
-	printf "%2d%%" $cpu
+printf "%2d%%" $cpu
