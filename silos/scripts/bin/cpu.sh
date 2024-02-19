40fd7a39db4bb645528e87bf0de4e7b968b4e34c
Adam Rutkowski <hq@mtod.org>
2024-02-19 13:37:15 +0100

    Update scripts

--- silos/scripts/bin/cpu.sh
+++ silos/scripts/bin/cpu.sh
@@ -6,6 +6,12 @@ sleep 0.5
 read -r cpu a b c idle rest < /proc/stat
 total=$((a+b+c+idle))
 cpu=$((100*( (total-prevtotal) - (idle-previdle) ) / (total-prevtotal) ))
-temp=$(sensors | grep -i '\(Tdie\|CPU\)' | awk '{print $2 " "}' | tr -d '\n')
+
+if [[ "$HOSTNAME" == "carb" ]]; then
+    temp=$(sensors | grep 'CPU' | awk '{print $2 " "}' | tr -d '\n')
+fi
+if [[ "$HOSTNAME" == "yharnam" ]]; then
+    temp=$(sensors | grep 'Tdie' | awk '{print $2 " "}' | tr -d '\n')
+fi
 printf "%2d%% %s" "$cpu" "$temp"
 
