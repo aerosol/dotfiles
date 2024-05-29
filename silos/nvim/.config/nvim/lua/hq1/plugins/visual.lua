6ebc6d37d88d68b8a6d736969e5cfc728cae90b7
Adam Rutkowski <hq@mtod.org>
2024-05-29 10:22:00 +0200

    Update nvim

--- silos/nvim/.config/nvim/lua/hq1/plugins/visual.lua
+++ silos/nvim/.config/nvim/lua/hq1/plugins/visual.lua
@@ -3,7 +3,10 @@ return {
 		"brenoprata10/nvim-highlight-colors",
 		config = function()
 			require("nvim-highlight-colors").setup({
-				render = "background",
+				render = "virtual",
+				virtual_symbol = '',
+				virtual_symbol_prefix = ' ',
+				virtual_symbol_position = 'eow',
 				enable_named_colors = true,
 				enable_tailwind = true,
 			})
