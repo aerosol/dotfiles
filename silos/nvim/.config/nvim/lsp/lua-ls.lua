891bc597fc8efb0aba723833ea525ba684b034bf
Adam Rutkowski <hq@mtod.org>
2025-07-29 21:11:16 +0200

    Update nvim

--- silos/nvim/.config/nvim/lsp/lua-ls.lua
+++ silos/nvim/.config/nvim/lsp/lua-ls.lua
@@ -17,4 +17,21 @@ return {
 	},
 	single_file_support = true,
 	log_level = vim.lsp.protocol.MessageType.Warning,
+	settings = {
+		Lua = {
+			runtime = {
+				version = "LuaJIT",
+			},
+			diagnostics = {
+				globals = { "vim" },
+			},
+			workspace = {
+				library = vim.api.nvim_get_runtime_file("", true),
+				checkThirdParty = false,
+			},
+			telemetry = {
+				enable = false,
+			},
+		},
+	},
 }
